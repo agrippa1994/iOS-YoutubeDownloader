b76361393d60d144165b629d52fcd7216a50541d
agrippa1994 <agrippa1994@icloud.com>
2016-12-10 00:47:35 +0100

    save persistence file whenever the app is closed

--- YoutubeDownloader/AppDelegate.swift
+++ YoutubeDownloader/AppDelegate.swift
@@ -51,8 +51,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
+        do {
+            try Persistence.shared.save()
+        } catch {
+            NSLog("Error while saving CoreData SQL file: \(error)")
+        }
     }
 
 
